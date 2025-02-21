bc71ab637a9788476085f47d5e898234a88c8d6f
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-02-21 19:21:11 -0300

    Inicial

---
+++ src/main/java/br/gigliarly/torneio_brawhalla_api/TorneioBrawhallaApiApplication.java
@@ -0,0 +1,13 @@
+package br.gigliarly.torneio_brawhalla_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneioBrawhallaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TorneioBrawhallaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/gigliarly/torneio_brawhalla_api/TorneioBrawhallaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gigliarly.torneio_brawhalla_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneioBrawhallaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
